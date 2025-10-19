077e976874f86d89a1fcc3827755f36c793c8409
FabianinhaClr <claraantonelo194@gmail.com>
2025-10-19 14:20:47 -0300

    ajusts wsgi e sttings

--- projeto/wsgi.py
+++ projeto/wsgi.py
@@ -8,9 +8,23 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.http import HttpResponse
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto.settings')
 
+# Cria a aplicação padrão do Django
 application = get_wsgi_application()
+
+# Adiciona uma rota /ping simples (útil para health check)
+def ping_application(environ, start_response):
+    if environ.get('PATH_INFO') == '/ping':
+        status = '200 OK'
+        headers = [('Content-Type', 'text/plain')]
+        start_response(status, headers)
+        return [b'Ping response']
+    # Caso contrário, processa normalmente pelo Django
+    return application(environ, start_response)
+
+# O que o Vercel (ou servidor WSGI) usará como entrada
+app = ping_application
